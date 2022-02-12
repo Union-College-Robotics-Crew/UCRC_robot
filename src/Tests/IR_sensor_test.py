81fa12bee06e34b1220d2eff4b592ffb311cdd04
Edwin Garcia-Flores <47260584+egarcia-flores@users.noreply.github.com>
2022-02-12 14:33:04 -0500

    Update IR_Sensor_test.py

--- src/Tests/IR_sensor_test.py
+++ src/Tests/IR_sensor_test.py
@@ -1,20 +1,23 @@
-class IR_sensor_tests:
+from src import config
+from src import ir_sensor
 
-	ir1 = IR_sensor(lAng_ir)
-	ir2 = IR_sensor(rAng_ir)
-	ir3 = IR_sensor(l_ir)
-	ir4 = IR_sensor(r_ir)
-	ir5 = IR_sensor(lFwd_ir)
-	ir6 = IR_sensor(rFwd_ir)
+class IR_sensor_tests:
+	def __init__(self):
+		self.ir1 = IR_sensor(config.lAng_ir)
+		self.ir2 = IR_sensor(config.rAng_ir)
+		self.ir3 = IR_sensor(config.l_ir)
+		self.ir4 = IR_sensor(config.r_ir)
+		self.ir5 = IR_sensor(config.lFwd_ir)
+		self.ir6 = IR_sensor(config.rFwd_ir)
 	
 
 	def test_ir(amt_readings):
 		for i in range(amt_readings):
-      print("ir1: ", ir1)
-			print("ir2: ", ir2)
-			print("ir3: ", ir3)
-			print("ir4: ", ir4)
-			print("ir5: ", ir5)
-			print("ir6: ", ir6)
-      
-  test_ir(1000)
+      			print("ir1: ", self.ir1)
+			print("ir2: ", self.ir2)
+			print("ir3: ", self.ir3)
+			print("ir4: ", self.ir4)
+			print("ir5: ", self.ir5)
+			print("ir6: ", self.ir6)
+IR_tests = IR_sensor_tests()
+print(IR_tests.test_ir(1000))
