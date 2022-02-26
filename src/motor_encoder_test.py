0398a81305318505a11cec19600170e35c5aeb85
Luka Mgaloblishvili <mgalobll@union.edu>
2022-02-26 14:47:54 -0500

    Added test for reading speed of both motors - both_motor_speed()

--- src/motor_encoder_test.py
+++ src/motor_encoder_test.py
@@ -10,7 +10,7 @@ class MotorEncoderTest:
     def __init__(self) -> None:
         pass
 
-    def motor_speed(iterations):
+    def single_motor_speed(iterations):
         test_motor = Motor(config.test_motorKit, config.pin_D5, config.pin_D6)
         speed = 0
         actual_speed = 0
@@ -35,6 +35,39 @@ class MotorEncoderTest:
                 speed -= 0.1
 
             readings += 1
+
+    def both_motor_speed(iterations, interval):
+        test_motor1 = Motor(config.l_motorKit, config.pin_D5, config.pin_D6)
+        test_motor2 = Motor(config.r_motorKit, config.pind_D9, config.pin_D10)
+        speed = 0
+        actual_speed1 = 0
+        actual_speed2 = 0
+        time_passed = 0
+        readings = 0
+        while readings < iterations:
+            while speed < 1:
+                test_motor1.run(speed)
+                test_motor2.run(speed)
+                time.sleep(interval)
+                time_passed += interval
+                actual_speed1 = test_motor1.get_speed(interval)
+                actual_speed2 = test_motor2.get_speed(interval)
+                print((actual_speed1, actual_speed2, speed,))
+                speed += 0.1
+
+            speed = 1
+
+            while speed > 0:
+                test_motor1.run(speed)
+                test_motor2.run(speed)
+                time.sleep(interval)
+                time_passed += interval
+                actual_speed = test_motor1.get_speed(interval)
+                print((actual_speed1, actual_speed2, speed,))
+                speed -= 0.1
+
+            readings += 1
+
 
     # def encoder_position(iterations):
     #     test_motor = Motor(config.test_motorKit, config.pin_D5, config.pin_D6)
